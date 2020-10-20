b2dcc99b1bfa0bc052481f0a2d94b59581723b4e
Git-Fanfo <51414591+GitFanfo@users.noreply.github.com>
2020-10-20 17:14:03 -0500

    version 1.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import tasks from './sample/task.json';
-console.log(tasks);
+//import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component{
+import Formulario from './components/Formulario'
+//import './App.css'; //import tasks from './sample/task.json'; //console.log(tasks);
+/*{ this.state.tasks.map(e => <p key={e.id}>{e.title} - {e.done}</p>) }
+        <h3>BIENVENIDO</h3>
+      <p>Ingrese su nombre, ctm:</p>
+      <input placeholder="Escribe tu nombre aquí" name="identificacion" type="text" tabindex="2" required></input>      
+      */
 
-  state = {
-    tasks: tasks
-
-  }
-  render(){
-    return <div>
-      { this.state.tasks.map(e => <p key={e.id}>{e.title} - {e.done}</p>) }
+function App(){
+  return (
+    <div className="container mt-5">
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
     </div>
-  }
+  )
 }
-
 export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,60 @@
+import React, {Fragment, useState} from 'react';
+
+const Formulario = () => {
+
+const [datos, setDatos] = useState({
+    nombre: '',
+    apellido: ''
+})
+
+const handleInputChange = (event) => {
+    //console.log(event.target.value)
+    setDatos({
+        ...datos,
+        [event.target.name] : event.target.value
+    })
+}
+
+const enviarDatos = (event) => {
+    console.log("enviar");
+    event.preventDefault();
+    console.log(datos.nombre + ' ' + datos.apellido)
+}
+
+return(
+    <Fragment>
+        <h1>Formularios</h1>
+        <form className="row" onSubmit={enviarDatos} >
+            <div className="col-md-3">
+                <input
+                    placeholder="Ingrese su nombre."
+                    className="form-control"
+                    type="text"
+                    name="nombre"
+                    onChange={handleInputChange}>
+                </input>  
+            </div>
+            <div className="col-md-3">
+                <input
+                    placeholder="Ingrese su apellido."
+                    className="form-control"
+                    type="text"
+                    name="apellido"
+                    onChange={handleInputChange}>
+                </input>  
+            </div>
+            <div className="col-md-3">
+                <button
+                    className="btn btn-primary"
+                    type="button"
+                    >                    
+                    Enviar
+                </button>  
+            </div> 
+        </form>
+<h3>{datos.nombre} - {datos.apellido}</h3>
+    </Fragment>      
+    );
+}
+
+export default Formulario;
